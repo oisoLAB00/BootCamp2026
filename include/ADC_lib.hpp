e2edb19252152f38e30e2c15e1427cb90acbaa39
oisoLAB00 <oisoyuta.lab@gmail.com>
2026-04-30 02:52:01 +0900

    Arduinoのcoreを2→3へ移行, DMA関連, サーボライブラリ関連のエラー解消

--- include/ADC_lib.hpp
+++ include/ADC_lib.hpp
@@ -1,28 +1,40 @@
 #pragma once
+
 #include <Arduino.h>
+
+#include "esp_adc/adc_continuous.h"
+#include "esp_err.h"
 
 #define ADC_NUM 4
 #define BUF_SIZE 200
+
+// GPIO
 #define EMG_PIN_1 34
 #define EMG_PIN_2 35
 #define FSR_PIN_1 32
 #define FSR_PIN_2 33
 #define MODE_SW 22
+
 #define EMG_NUM 2
 
+// Threshold
 #define FSR_TH_VAL 100
 #define EMG_TH_OPEN_1 2000
 #define EMG_TH_CLOSE_2 2000
 
+// ADC ID
 #define ID_EMG1 0
 #define ID_EMG2 1
 #define ID_FSR1 2
 #define ID_FSR2 3
 
-//DMA設定
-#define CONVERSIONS_PER_PIN 10  // 1回の読み出しで各ピン何回サンプリングするか
-#define SAMPLING_FREQ 20000     // サンプリング周波数 20k[Hz]
+// DMA設定
+#define CONVERSIONS_PER_PIN 10
+#define SAMPLING_FREQ 20000
 #define READ_TIMEOUT_MS 10
+
+// DMA Buffer
+#define ADC_DMA_BUF_SIZE 256
 
 extern volatile bool is_EMG_close;
 extern volatile bool is_EMG_open;
@@ -31,65 +43,85 @@ extern volatile bool is_FSR2;
 
 class ADC_lib
 {
-    public :
-        ADC_lib()
+public:
+
+    ADC_lib()
+    {
+    }
+
+    ~ADC_lib()
+    {
+        adc_continuous_stop(adc_handle);
+        adc_continuous_deinit(adc_handle);
+
+        ADC_reset();
+        Reset_ADC_th();
+    }
+
+    void ADC_init();
+
+    void EMG_Calibration();
+
+    void update();
+
+    void set_ADC_val(
+        int emg_val1,
+        int emg_val2,
+        int fsr_val1,
+        int fsr_val2
+    );
+
+    int get_ADC_val(int id);
+
+private:
+
+    // =========================
+    // ADC DMA Handle
+    // =========================
+    adc_continuous_handle_t adc_handle = NULL;
+
+    // =========================
+    // ADC Value
+    // =========================
+    volatile short adc_val[ADC_NUM];
+
+    int adc_th[ADC_NUM];
+
+    short adc_buf[EMG_NUM][BUF_SIZE];
+
+    short emg_base[ADC_NUM];
+
+    int buf_index{0};
+
+    // =========================
+    // ADC GPIO
+    // =========================
+    uint8_t adc_pins[ADC_NUM] =
+    {
+        EMG_PIN_1,
+        EMG_PIN_2,
+        FSR_PIN_1,
+        FSR_PIN_2
+    };
+
+    // =========================
+    // Internal Functions
+    // =========================
+    void ADC_reset();
+
+    void cal_ADC_avg(int id);
+
+    void set_ADC_th(int id, int threshold);
+
+    void set_EMG_base(int base_1, int base_2);
+
+    void set_EMG_raw_data(int emg_val1, int emg_val2);
+
+    void Reset_ADC_th()
+    {
+        for(int i = 0; i < ADC_NUM; i++)
         {
-            //ADC_reset();
-            //Reset_emg_th();
+            adc_th[i] = 0;
         }
-
-        ~ADC_lib()
-        {
-            ADC_reset();
-            Reset_ADC_th();
-        }
-
-        void ADC_init()
-        {
-            /*
-            pinMode(EMG_PIN_1, INPUT);
-            pinMode(EMG_PIN_2, INPUT);
-            pinMode(FSR_PIN_1, INPUT);
-            pinMode(FSR_PIN_2, INPUT);
-            */
-            analogContinuousInit(adc_pins, ADC_NUM, CONVERSIONS_PER_PIN, SAMPLING_FREQ, NULL);
-            analogContinuousSetAtten(ADC_ATTEN_DB_12);
-            analogContinuousStart();
-            pinMode(MODE_SW, INPUT_PULLUP);
-            ADC_reset();
-            Reset_ADC_th();
-            set_ADC_th(ID_FSR1, FSR_TH_VAL);
-            set_ADC_th(ID_FSR2, FSR_TH_VAL);
-            set_ADC_th(ID_EMG1, EMG_TH_OPEN_1);
-            set_ADC_th(ID_EMG2, EMG_TH_CLOSE_2);
-        }
-
-        void EMG_Calibration();
-        void update();
-        void set_ADC_val(int emg_val1, int emg_val2, int fsr_val1, int fsr_val2);
-        int get_ADC_val(int id);
-        
-
-
-    private :
-        volatile short adc_val[ADC_NUM];
-        int adc_th[ADC_NUM];
-        short adc_buf[EMG_NUM][BUF_SIZE];
-        short emg_base[ADC_NUM];
-        int buf_index{0};
-        uint8_t adc_pins[ADC_NUM] = {EMG_PIN_1, EMG_PIN_2, FSR_PIN_1, FSR_PIN_2};
-
-        void ADC_reset();
-        void cal_ADC_avg(int id);
-        void Reset_ADC_th()
-        {
-            for(int i =0; i < ADC_NUM ; i++)
-            {
-                adc_th[i] = 0;
-            }
-        }
-        void set_ADC_th(int id, int threshold);
-        void set_EMG_base(int base_1, int base_2);
-        void set_EMG_raw_data(int emg_val1, int emg_val2);
+    }
 };
-
--- src/ADC_lib.cpp
+++ src/ADC_lib.cpp
@@ -1,21 +1,80 @@
 #include "ADC_lib.hpp"
 
+
+void ADC_lib::ADC_init()
+{
+    pinMode(MODE_SW, INPUT_PULLUP);
+
+    adc_continuous_handle_cfg_t adc_config;
+
+    adc_config.max_store_buf_size = 1024;
+    adc_config.conv_frame_size = ADC_DMA_BUF_SIZE;
+
+    ESP_ERROR_CHECK(
+        adc_continuous_new_handle(
+            &adc_config,
+            &adc_handle
+        )
+    );
+
+    adc_digi_pattern_config_t adc_pattern[ADC_NUM] = {};
+
+    // GPIO34 -> ADC1_CHANNEL_6
+    adc_pattern[ID_EMG1].atten = ADC_ATTEN_DB_12;
+    adc_pattern[ID_EMG1].channel = ADC_CHANNEL_6;
+    adc_pattern[ID_EMG2].unit = ADC_UNIT_1;
+    adc_pattern[ID_EMG1].bit_width = SOC_ADC_DIGI_MAX_BITWIDTH;
+
+    // GPIO35 -> ADC1_CHANNEL_7
+    adc_pattern[ID_EMG2].atten = ADC_ATTEN_DB_12;
+    adc_pattern[ID_EMG2].channel = ADC_CHANNEL_7;
+    adc_pattern[ID_EMG2].unit = ADC_UNIT_1;
+    adc_pattern[ID_EMG2].bit_width = SOC_ADC_DIGI_MAX_BITWIDTH;
+
+    // GPIO32 -> ADC1_CHANNEL_4
+    adc_pattern[ID_FSR1].atten = ADC_ATTEN_DB_12;
+    adc_pattern[ID_FSR1].channel = ADC_CHANNEL_4;
+    adc_pattern[ID_FSR1].unit = ADC_UNIT_1;
+    adc_pattern[ID_FSR1].bit_width = SOC_ADC_DIGI_MAX_BITWIDTH;
+
+    // GPIO33 -> ADC1_CHANNEL_5
+    adc_pattern[ID_FSR2].atten = ADC_ATTEN_DB_12;
+    adc_pattern[ID_FSR2].channel = ADC_CHANNEL_5;
+    adc_pattern[ID_FSR2].unit = ADC_UNIT_1;
+    adc_pattern[ID_FSR2].bit_width = SOC_ADC_DIGI_MAX_BITWIDTH;
+
+    adc_continuous_config_t dig_cfg;
+
+    dig_cfg.sample_freq_hz = SAMPLING_FREQ;
+    dig_cfg.conv_mode = ADC_CONV_SINGLE_UNIT_1;
+    dig_cfg.format = ADC_DIGI_OUTPUT_FORMAT_TYPE2;
+    dig_cfg.pattern_num = ADC_NUM;
+    dig_cfg.adc_pattern = adc_pattern;
+
+    ESP_ERROR_CHECK(
+        adc_continuous_config(adc_handle, &dig_cfg)
+    );
+
+    ESP_ERROR_CHECK(
+        adc_continuous_start(adc_handle)
+    );
+
+    ADC_reset();
+    Reset_ADC_th();
+    set_ADC_th(ID_FSR1, FSR_TH_VAL);
+    set_ADC_th(ID_FSR2, FSR_TH_VAL);
+    set_ADC_th(ID_EMG1, EMG_TH_OPEN_1);
+    set_ADC_th(ID_EMG2, EMG_TH_CLOSE_2);
+}
+
+
 void ADC_lib::set_ADC_val(int emg_val1, int emg_val2, int fsr_val1, int fsr_val2)
 {
-
     adc_val[ID_FSR1] = fsr_val1;
     adc_val[ID_FSR2] = fsr_val2;
 
-    if(fsr_val1 > FSR_TH_VAL){
-        is_FSR1 = true;
-    }else{
-        is_FSR1 = false;
-    }
-    if(fsr_val2 > FSR_TH_VAL){
-        is_FSR2 = true;
-    }else{
-        is_FSR2 = false;
-    }
+    is_FSR1 = (fsr_val1 > FSR_TH_VAL);
+    is_FSR2 = (fsr_val2 > FSR_TH_VAL);
 
     adc_buf[ID_EMG1][buf_index] = emg_val1;
     adc_buf[ID_EMG2][buf_index] = emg_val2;
@@ -24,16 +83,12 @@ void ADC_lib::set_ADC_val(int emg_val1, int emg_val2, int fsr_val1, int fsr_val2
 
     cal_ADC_avg(ID_EMG1);
     cal_ADC_avg(ID_EMG2);
-    
-    if(adc_val[ID_EMG1] > adc_th[ID_EMG1])
-        is_EMG_open = true;
-    else
-        is_EMG_open = false;
-    if(adc_val[ID_EMG2] > adc_th[ID_EMG2])
-        is_EMG_close = true;
-    else
-        is_EMG_close = false;
 
+    is_EMG_open =
+        (adc_val[ID_EMG1] > adc_th[ID_EMG1]);
+
+    is_EMG_close =
+        (adc_val[ID_EMG2] > adc_th[ID_EMG2]);
 }
 
 int ADC_lib::get_ADC_val(int id)
@@ -48,10 +103,15 @@ void ADC_lib::set_ADC_th(int id, int threshold)
 
 void ADC_lib::ADC_reset()
 {
-    adc_val[ID_EMG1] = adc_val[ID_EMG2] =  0;
+    for(int i = 0; i < ADC_NUM; i++)
+    {
+        adc_val[i] = 0;
+    }
+
     for(int size = 0; size < BUF_SIZE; size++)
     {
-        for(int id=0; id < EMG_NUM; id++){
+        for(int id = 0; id < EMG_NUM; id++)
+        {
             adc_buf[id][size] = 0;
         }
     }
@@ -60,24 +120,27 @@ void ADC_lib::ADC_reset()
 void ADC_lib::cal_ADC_avg(int id)
 {
     long val = 0;
+
     for(int size = 0; size < BUF_SIZE; size++)
     {
-        val += (long)adc_buf[id][size];
+        val += adc_buf[id][size];
     }
+
     adc_val[id] = (short)(val / BUF_SIZE);
-    
 }
 
 void ADC_lib::EMG_Calibration()
 {
     for(int time = 0; time < BUF_SIZE; time++)
     {
-        set_EMG_raw_data(analogRead(ID_EMG1), analogRead(ID_EMG2));
+        set_EMG_raw_data(analogRead(EMG_PIN_1), analogRead(EMG_PIN_2));
+
         delay(10);
     }
-    set_EMG_base(get_ADC_val(ID_EMG1), get_ADC_val(ID_EMG2));
 
+    set_EMG_base(get_ADC_val(ID_EMG1), get_ADC_val(ID_EMG2));
 }
+
 void ADC_lib::set_EMG_raw_data(int emg_val1, int emg_val2)
 {
     adc_buf[ID_EMG1][buf_index] = emg_val1;
@@ -88,54 +151,78 @@ void ADC_lib::set_EMG_raw_data(int emg_val1, int emg_val2)
 
     buf_index++;
 
-    if (buf_index >= BUF_SIZE) {
+    if(buf_index >= BUF_SIZE)
+    {
         buf_index = 0;
     }
 }
 
-void ADC_lib::set_EMG_base(int base_1, int base_2)
+void ADC_lib::set_EMG_base( int base_1, int base_2)
 {
     emg_base[ID_EMG1] = base_1;
     emg_base[ID_EMG2] = base_2;
 }
 
-void ADC_lib::update() {
-    adc_continuous_data_t *result = NULL;
-    uint32_t count = 0;
+void ADC_lib::update()
+{
+    uint8_t result[ADC_DMA_BUF_SIZE];
 
-    // DMAバッファからデータを取得
-    if (analogContinuousRead(&result, &count, READ_TIMEOUT_MS)) {
-        for (int i = 0; i < count; i++) {
-            int raw_val = result[i].avg_read_raw;
-            int pin = result[i].pin;
+    uint32_t ret_num = 0;
 
-            // ピン番号からIDを特定して格納
+    esp_err_t ret = adc_continuous_read(adc_handle, result, ADC_DMA_BUF_SIZE, &ret_num, READ_TIMEOUT_MS);
+
+    if(ret == ESP_OK)
+    {
+        for(int i = 0; i < ret_num; i += SOC_ADC_DIGI_RESULT_BYTES)
+        {
+            adc_digi_output_data_t* p = (adc_digi_output_data_t*)&result[i];
+
+            uint32_t channel = p->type2.channel;
+            uint32_t data = p->type2.data;
+
             int id = -1;
-            if (pin == EMG_PIN_1) id = ID_EMG1;
-            else if (pin == EMG_PIN_2) id = ID_EMG2;
-            else if (pin == FSR_PIN_1) id = ID_FSR1;
-            else if (pin == FSR_PIN_2) id = ID_FSR2;
 
-            if (id != -1) {
-                if (id == ID_EMG1 || id == ID_EMG2) {
-                    // EMGは既存の移動平均ロジックへ
-                    adc_buf[id][buf_index] = raw_val;
-                } else {
-                    // FSRはそのまま格納（または別途平滑化）
-                    adc_val[id] = raw_val;
+            switch(channel)
+            {
+                case ADC_CHANNEL_6:
+                    id = ID_EMG1;
+                    break;
+
+                case ADC_CHANNEL_7:
+                    id = ID_EMG2;
+                    break;
+
+                case ADC_CHANNEL_4:
+                    id = ID_FSR1;
+                    break;
+
+                case ADC_CHANNEL_5:
+                    id = ID_FSR2;
+                    break;
+            }
+
+            if(id != -1)
+            {
+                if(id == ID_EMG1 || id == ID_EMG2)
+                {
+                    adc_buf[id][buf_index] =  data;
+                }
+                else
+                {
+                    adc_val[id] = data;
                 }
             }
         }
-        
-        // 既存の平均化・判定ロジック
+
         buf_index = (buf_index + 1) % BUF_SIZE;
+
         cal_ADC_avg(ID_EMG1);
         cal_ADC_avg(ID_EMG2);
 
-        // 判定フラグ更新
         is_FSR1 = (adc_val[ID_FSR1] > FSR_TH_VAL);
         is_FSR2 = (adc_val[ID_FSR2] > FSR_TH_VAL);
+
         is_EMG_open = (adc_val[ID_EMG1] > adc_th[ID_EMG1]);
-        is_EMG_close = (adc_val[ID_EMG2] > adc_th[ID_EMG2]);
+        is_EMG_close =(adc_val[ID_EMG2] > adc_th[ID_EMG2]);
     }
 }
